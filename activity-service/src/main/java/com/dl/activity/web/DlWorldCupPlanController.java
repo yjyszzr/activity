b794e73dd2003c67b8becaddfb969aa9e68b70c3
yjyszzr <1053195207@qq.com>
2018-06-11 19:02:02 +0800

    modified
    Change-Id: Ifdf0b2cf3d2e19339b1f722ea31b065d1a803688
    Signed-off-by: yjyszzr <1053195207@qq.com>

--- activity-service/src/main/java/com/dl/activity/web/DlWorldCupPlanController.java
+++ activity-service/src/main/java/com/dl/activity/web/DlWorldCupPlanController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dl.activity.dto.DlWorldCupContryDTO;
 import com.dl.activity.dto.GuessingCompetitionDTO;
 import com.dl.activity.dto.WCPlanDTO;
+import com.dl.activity.enums.ActivityEnums;
 import com.dl.activity.model.DlWorldCupContry;
 import com.dl.activity.model.DlWorldCupPlan;
 import com.dl.activity.param.EmptyParam;
