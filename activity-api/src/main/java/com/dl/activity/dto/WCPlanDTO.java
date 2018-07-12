ab29d235cbb8290c803919b9c1088dac0ab60e77
liweijie <liweijie@caixiaomi.com>
2018-07-12 10:50:16 +0800

    提交活动相关

--- activity-api/src/main/java/com/dl/activity/dto/WCPlanDTO.java
+++ activity-api/src/main/java/com/dl/activity/dto/WCPlanDTO.java
@@ -1,9 +1,10 @@
 package com.dl.activity.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
@@ -11,25 +12,56 @@ public class WCPlanDTO {
 
 	@ApiModelProperty("竞猜时间")
 	private String addTime;
+
 	@ApiModelProperty("竞猜16强选项")
 	private List<WCPlanCellDTO> plan16 = new ArrayList<WCPlanCellDTO>(0);
+
 	@ApiModelProperty("是否猜种0待开奖，1命中，2未命中")
 	private Integer rst16 = 0;
+
+	@ApiModelProperty("16强奖金")
+	private String rst16Amount;
+
 	@ApiModelProperty("竞猜8强选项")
 	private List<WCPlanCellDTO> plan8 = new ArrayList<WCPlanCellDTO>(0);;
+
 	@ApiModelProperty("是否猜种0待开奖，1命中，2未命中")
 	private Integer rst8 = 0;
+
+	@ApiModelProperty("8强奖金")
+	private String rst8Amount;
+
 	@ApiModelProperty("竞猜4强选项")
 	private List<WCPlanCellDTO> plan4 = new ArrayList<WCPlanCellDTO>(0);;
+
 	@ApiModelProperty("是否猜种0待开奖，1命中，2未命中")
 	private Integer rst4 = 0;
+
+	@ApiModelProperty("4强奖金")
+	private String rst4Amount;
+
 	@ApiModelProperty("竞猜冠亚军选项")
 	private List<WCPlanCellDTO> plan2 = new ArrayList<WCPlanCellDTO>(0);;
+
 	@ApiModelProperty("是否猜种0待开奖，1命中，2未命中")
 	private Integer rst2 = 0;
+
+	@ApiModelProperty("冠亚军奖金")
+	private String rst2Amount;
+
 	@ApiModelProperty("竞猜冠军选项")
 	private WCPlanCellDTO plan1 = new WCPlanCellDTO();
+
 	@ApiModelProperty("是否猜种0待开奖，1命中，2未命中")
 	private Integer rst1 = 0;
-	
+
+	@ApiModelProperty("冠军奖金")
+	private String rst1Amount;
+
+	@ApiModelProperty("是否猜种0待开奖，1命中，2未命中")
+	private Integer rstAllTrue = 0;
+
+	@ApiModelProperty("终极奖金")
+	private String rstAllTrueAmount;
+
 }
--- activity-service/src/main/java/com/dl/activity/service/DlWorldCupPlanService.java
+++ activity-service/src/main/java/com/dl/activity/service/DlWorldCupPlanService.java
@@ -15,6 +15,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import tk.mybatis.mapper.entity.Condition;
+
 import com.dl.activity.dao.DlWorldCupPlanMapper;
 import com.dl.activity.dto.WCContryDTO;
 import com.dl.activity.dto.WCPlanCellDTO;
@@ -24,17 +26,15 @@ import com.dl.base.service.AbstractService;
 import com.dl.base.util.DateUtil;
 import com.dl.base.util.SessionUtil;
 
-import tk.mybatis.mapper.entity.Condition;
-
 @Service
 // @Transactional(value = "transactionManager1")
 public class DlWorldCupPlanService extends AbstractService<DlWorldCupPlan> {
 	@Resource
 	private DlWorldCupPlanMapper dlWorldCupPlanMapper;
-	
+
 	@Resource
 	private DlWorldCupContryService dlWorldCupContryService;
-	
+
 	@Transactional(value = "transactionManager1")
 	public Integer saveWorldCupPlan(String palnStr) {
 		Integer userId = SessionUtil.getUserId();
@@ -50,16 +50,17 @@ public class DlWorldCupPlanService extends AbstractService<DlWorldCupPlan> {
 
 	/**
 	 * 获取投注列表
+	 * 
 	 * @param userId
 	 * @return
 	 */
-	public List<WCPlanDTO> worldCupPlanList(Integer userId){
+	public List<WCPlanDTO> worldCupPlanList(Integer userId) {
 		List<WCPlanDTO> dtos = new ArrayList<WCPlanDTO>(0);
 		List<DlWorldCupPlan> worldCupPlanList = dlWorldCupPlanMapper.getWorldCupPlanList(userId);
-		if(CollectionUtils.isNotEmpty(worldCupPlanList)) {
+		if (CollectionUtils.isNotEmpty(worldCupPlanList)) {
 			Map<Integer, WCContryDTO> contryMap = dlWorldCupContryService.wcContryMap();
 			dtos = new ArrayList<WCPlanDTO>(worldCupPlanList.size());
-			for(DlWorldCupPlan plan: worldCupPlanList) {
+			for (DlWorldCupPlan plan : worldCupPlanList) {
 				WCPlanDTO dto = new WCPlanDTO();
 				Integer addTime = plan.getAddTime();
 				LocalDateTime localDateTime = LocalDateTime.ofEpochSecond(Long.valueOf(addTime), 0, ZoneOffset.ofHours(8));
@@ -68,57 +69,65 @@ public class DlWorldCupPlanService extends AbstractService<DlWorldCupPlan> {
 				String planJson = plan.getPlanJson();
 				this.parsePlanJson(planJson, dto, contryMap);
 				Integer isOpen = plan.getIsOpen();
-				if(1==isOpen) {
-					dto.setRst1(plan.getStatusGj());
+				if (1 == isOpen) {
 					dto.setRst16(plan.getStatus16());
-					dto.setRst2(plan.getStatusGyj());
-					dto.setRst4(plan.getStatus4());
 					dto.setRst8(plan.getStatus8());
+					dto.setRst4(plan.getStatus4());
+					dto.setRst2(plan.getStatusGyj());
+					dto.setRst1(plan.getStatusGj());
+					dto.setRstAllTrue(plan.getRewardTimeAllTrue());
+					dto.setRst16Amount(plan.getRewardAmount16().toString());
+					dto.setRst8Amount(plan.getRewardAmount8().toString());
+					dto.setRst4Amount(plan.getRewardAmount4().toString());
+					dto.setRst2Amount(plan.getRewardAmountGyj().toString());
+					dto.setRst1Amount(plan.getRewardAmountGj().toString());
+					dto.setRstAllTrueAmount(plan.getRewardAmountAllTrue().toString());
 				}
 				dtos.add(dto);
 			}
 		}
 		return dtos;
 	}
-	//16|A1:396,A2:543,B1:424,B2:1044,C1:375,C2:409,D1:413,D2:366,E1:417,E2:420,F1:377,F2:403,G1:363,G2:425,H1:392,H2:410;8|A1:396,A2:424,B1:375,B2:413,C1:417,C2:377,D1:363,D2:392;4|A1:396,A2:375,B1:417,B2:363;2|A1:396,A2:417;1|A1:396
+
+	// 16|A1:396,A2:543,B1:424,B2:1044,C1:375,C2:409,D1:413,D2:366,E1:417,E2:420,F1:377,F2:403,G1:363,G2:425,H1:392,H2:410;8|A1:396,A2:424,B1:375,B2:413,C1:417,C2:377,D1:363,D2:392;4|A1:396,A2:375,B1:417,B2:363;2|A1:396,A2:417;1|A1:396
 	private void parsePlanJson(String planJson, WCPlanDTO planDto, Map<Integer, WCContryDTO> contryMap) {
 		String[] cells = planJson.split(";");
-		for(String cell: cells) {
+		for (String cell : cells) {
 			String[] split = cell.split("\\|");
 			int parseInt = Integer.parseInt(split[0]);
-			if(1 == parseInt) {
+			if (1 == parseInt) {
 				String[] contrys = split[1].split(":");
 				Integer contryId = Integer.parseInt(contrys[1]);
 				WCContryDTO contryDto = contryMap.get(contryId);
 				WCPlanCellDTO dto = new WCPlanCellDTO(contryId, contryDto.getContryName());
 				dto.setIsGet(0);
 				String is1 = contryDto.getIs1();
-				if("A1".equals(is1)) {
+				if ("A1".equals(is1)) {
 					dto.setIsGet(1);
 				}
 				planDto.setPlan1(dto);
 				continue;
 			}
 			List<WCPlanCellDTO> plans = null;
-			if(16 == parseInt) {
+			if (16 == parseInt) {
 				plans = new ArrayList<WCPlanCellDTO>(16);
 				planDto.setPlan16(plans);
-			}else if(8 == parseInt) {
+			} else if (8 == parseInt) {
 				plans = new ArrayList<WCPlanCellDTO>(8);
 				planDto.setPlan8(plans);
-			}else if(4 == parseInt) {
+			} else if (4 == parseInt) {
 				plans = new ArrayList<WCPlanCellDTO>(4);
 				planDto.setPlan4(plans);
-			}else if(2 == parseInt) {
+			} else if (2 == parseInt) {
 				plans = new ArrayList<WCPlanCellDTO>(2);
 				planDto.setPlan2(plans);
 			}
 			String[] contrys = split[1].split(",");
-			for(String contry: contrys) {
+			for (String contry : contrys) {
 				String[] split2 = contry.split(":");
 				Integer contryId = Integer.parseInt(split2[1]);
 				WCContryDTO contryDto = contryMap.get(contryId);
-				if(contryDto == null) {
+				if (contryDto == null) {
 					System.out.println(contryId);
 				}
 				WCPlanCellDTO dto = new WCPlanCellDTO(contryId, contryDto.getContryName());
@@ -130,33 +139,34 @@ public class DlWorldCupPlanService extends AbstractService<DlWorldCupPlan> {
 	}
 
 	/**
-	 * 获取猜中的值 
+	 * 获取猜中的值
 	 */
 	private Integer getIsGet(WCContryDTO contryDto, int parseInt) {
 		Integer isGet = 0;
-		if(16 == parseInt) {
+		if (16 == parseInt) {
 			String is16 = contryDto.getIs16();
-			if(StringUtils.isNotBlank(is16) && !is16.equals("0")) {
+			if (StringUtils.isNotBlank(is16) && !is16.equals("0")) {
 				isGet = 1;
 			}
-		}else if(8 == parseInt) {
+		} else if (8 == parseInt) {
 			String is8 = contryDto.getIs8();
-			if(StringUtils.isNotBlank(is8) && !is8.equals("0")) {
+			if (StringUtils.isNotBlank(is8) && !is8.equals("0")) {
 				isGet = 1;
 			}
-		}else if(4 == parseInt) {
+		} else if (4 == parseInt) {
 			String is4 = contryDto.getIs4();
-			if(StringUtils.isNotBlank(is4) && !is4.equals("0")) {
+			if (StringUtils.isNotBlank(is4) && !is4.equals("0")) {
 				isGet = 1;
 			}
-		}else if(2 == parseInt) {
+		} else if (2 == parseInt) {
 			String is2 = contryDto.getIs2();
-			if(StringUtils.isNotBlank(is2) && !is2.equals("0")) {
+			if (StringUtils.isNotBlank(is2) && !is2.equals("0")) {
 				isGet = 1;
 			}
 		}
 		return isGet;
 	}
+
 	public BigDecimal findAllOrderAmount(Integer userId, Integer startTime) {
 		return dlWorldCupPlanMapper.findAllOrderAmount(userId, startTime);
 	}
