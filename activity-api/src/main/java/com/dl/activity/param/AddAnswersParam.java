cd65d8c0d5d7e206c27c3cf1f40096b969faf554
liweijie <liweijie@caixiaomi.com>
2018-07-26 17:42:33 +0800

    添加 答题接口

--- activity-api/src/main/java/com/dl/activity/param/AddAnswersParam.java
+++ activity-api/src/main/java/com/dl/activity/param/AddAnswersParam.java
@@ -1,7 +1,13 @@
 package com.dl.activity.param;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class AddAnswersParam {
+	@ApiModelProperty(value = "赛事Id")
+	public Integer matchId;
+
+	@ApiModelProperty(value = "答案列表")
+	public String answers;
 }
