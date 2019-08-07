42a269af48ef9854dac2f1ae9ee4ccfe944492af
lifeilong <lifeilong@caixiaomi.com>
2019-08-07 11:37:25 +0800

    xg

--- activity-service/src/main/java/com/dl/activity/model/ActivityTgDTO.java
+++ activity-service/src/main/java/com/dl/activity/model/ActivityTgDTO.java
@@ -1,5 +1,6 @@
 package com.dl.activity.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -10,7 +11,7 @@ import lombok.Data;
 
 @Data
 public class ActivityTgDTO {
-	private List<Activity> activity;//活动基本信息
+	private List<Activity> activity = new ArrayList<Activity>();//活动基本信息
 	private ActivityUserInfo activityUserInfo;//活动用户数据
 	private List<ActivityConfig> acitvityBl;//伯乐将挡位
 	private List<ActivityConfig> acitvityRy;//荣耀将挡位
